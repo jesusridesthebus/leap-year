eefbf0c7795457e2939c61e0d6fafb8a75ac7cf2
Student <example@example.com>
2019-04-03 09:18:08 -0700

    add js logic to return a leap year from user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 $(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
