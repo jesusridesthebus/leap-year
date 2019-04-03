feda63688e1557ca889a9cce5431dae130854b8f
Student <example@example.com>
2019-04-03 09:06:17 -0700

    add basic html input form, add js to retrieve user input from form

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
